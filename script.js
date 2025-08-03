1eb010433793c3d01982722a2e22915f70c5eeda
Shaik Rahil <rahil.ahmed.sk@gmail.com>
2025-08-03 22:51:57 +0530

    Intial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+    },
+  },
+};
